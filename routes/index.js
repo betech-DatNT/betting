30e4f2957e430be9d89152136d95257e25260bc7
datnt <datnt@bisync.jp>
2020-12-03 13:15:02 +0700

    [issue_557]  Update winston package.

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('', { title: 'this admin'});
+  res.render('backend/home/index', { title: 'this admin'});
 });
 
 module.exports = router;
